cf55bddf99bc60c41532387a7cd8b9998ed06382
박일규 <repeater1384@naver.com>
2022-11-02 14:08:27 +0900

    [Add] First

---
+++ FirstPJ_SpringBoot/src/main/java/com/repeater/boot/FirstPjSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.repeater.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstPjSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstPjSpringBootApplication.class, args);
+	}
+
+}
